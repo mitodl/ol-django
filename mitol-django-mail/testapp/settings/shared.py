b2423e854b7113e83c9244f4e413aa8a8927015f
Nathan Levesque <rhysyngsun@gmail.com>
2020-09-16 10:18:40 -0400

    Added support to tag and automatically release

--- mitol-django-mail/testapp/settings/shared.py
+++ mitol-django-mail/testapp/settings/shared.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "0541x5@jg^ulh@1gk&9swit)b_@z5by#q*-6ofig@rv!al#1)="
+SECRET_KEY = "TESTAPP_SECRET"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
