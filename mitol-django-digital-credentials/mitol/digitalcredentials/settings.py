ad93c2c942aec255fdd47b31d5c247dbb17a743b
Nathan Levesque <rhysyngsun@gmail.com>
2021-02-09 09:09:26 -0500

    Make digital credentials settings optional

--- mitol-django-digital-credentials/mitol/digitalcredentials/settings.py
+++ mitol-django-digital-credentials/mitol/digitalcredentials/settings.py
@@ -6,10 +6,10 @@ from mitol.common.envs import get_string
 MITOL_DIGITAL_CREDENTIALS_VERIFY_SERVICE_BASE_URL = get_string(
     name="MITOL_DIGITAL_CREDENTIALS_VERIFY_SERVICE_BASE_URL",
     description="Base URL for sing-and-verify service to call for digital credentials",
-    required=True,
+    required=False,
 )
 MITOL_DIGITAL_CREDENTIALS_HMAC_SECRET = get_string(
     name="MITOL_DIGITAL_CREDENTIALS_HMAC_SECRET",
     description="HMAC secret to sign digital credentials requests with",
-    required=True,
+    required=False,
 )
