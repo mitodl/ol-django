1d8be5d5934345ad079c95c33c55abf7798c82ac
nathan <nathan@nathan-lev>
2021-06-23 17:11:21 -0400

    Release mitol-django-common/v1.0.0

--- src/mitol/common/CHANGELOG.md
+++ src/mitol/common/CHANGELOG.md
@@ -6,6 +6,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.0.0] - 2021-06-23
+
 ### Added
 - Support for Python 3.9
 
--- src/mitol/common/__init__.py
+++ src/mitol/common/__init__.py
@@ -1,5 +1,5 @@
 """ mitol.common """
 default_app_config = "mitol.common.apps.CommonApp"
 
-__version__ = "0.7.0"
+__version__ = "1.0.0"
 __distributionname__ = "mitol-django-common"
