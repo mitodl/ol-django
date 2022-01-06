d52f68b381a43d251015b5e6ad2c1cb2c6a83838
Abdurrehman <93309234+MAbdurrehman12@users.noreply.github.com>
2022-01-06 21:19:50 +0500

    fix: replacing url with re_path (#50)
    * fix: replacing url with re_path
    
    * django version constraint update in requirements.txt

--- src/mitol/mail/urls.py
+++ src/mitol/mail/urls.py
@@ -1,6 +1,6 @@
 """URL configurations for mail"""
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import re_path
 
 from mitol.mail.views import EmailDebuggerView
 
@@ -8,5 +8,5 @@ urlpatterns = []
 
 if getattr(settings, "MITOL_MAIL_ENABLE_EMAIL_DEBUGGER", False):  # pragma: no cover
     urlpatterns += [
-        url(r"^__emaildebugger__/$", EmailDebuggerView.as_view(), name="email-debugger")
+        re_path(r"^__emaildebugger__/$", EmailDebuggerView.as_view(), name="email-debugger")
     ]
