ab521023e628bfb023109021286d97f6bc485d49
Umar Asghar <mrumarasghar@gmail.com>
2022-05-10 15:54:57 +0500

    Implement a decorator that performs jitter on max_age

---
+++ src/mitol/common/decorators.py
@@ -0,0 +1,25 @@
+import random
+
+from django.utils.cache import patch_cache_control
+from django.views.utils import get_max_age
+
+
+def cache_control_max_age_jitter(**kwargs):
+    def _cache_controller(viewfunc):
+        @wraps(viewfunc)
+        def _cache_controlled(request, *args, **kwargs):
+            # Ensure argument looks like a request.
+            if not hasattr(request, "META"):
+                raise TypeError(
+                    "cache_control_max_age_jitter didn't receive an HttpRequest. If you are "
+                    "decorating a classmethod, be sure to use "
+                     "@method_decorator."
+                )
+            response = viewfunc(request, *args, **kwargs)
+            max_age = get_max_age(response)
+            # add random delay upto 5 minutes
+            kwargs["max_age"] = max_age + random.randint(1, 600)
+            patch_cache_control(response, **kwargs)
+            return response
+        return _cache_controlled
+    return _cache_controller
