94e90ee8a6e38e7e3639fb432d61fd192f2142cc
Anna Gavrilman <annagav@mit.edu>
2022-06-01 10:16:03 -0400

    Adding Auditable model and admin

--- src/mitol/common/models.py
+++ src/mitol/common/models.py
@@ -38,6 +38,65 @@ class TimestampedModel(Model):
         abstract = True
 
 
+class AuditableModel(Model):
+    """An abstract base class for auditable models"""
+
+    class Meta:
+        abstract = True
+
+    def to_dict(self):
+        """
+        Returns:
+            dict:
+                A serialized representation of the model object
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def objects_for_audit(cls):
+        """
+        Returns the correct model manager for the auditable model. This defaults to `objects`, but if
+        a different manager is needed for any reason (for example, if `objects` is changed to a manager
+        that applies some default filters), it can be overridden.
+
+        Returns:
+             django.db.models.manager.Manager: The correct model manager for the auditable model
+        """
+        return cls.objects
+
+    @classmethod
+    def get_audit_class(cls):
+        """
+        Returns:
+            class of Model:
+                A class of a Django model used as the audit table
+        """
+        raise NotImplementedError
+
+    @transaction.atomic
+    def save_and_log(self, acting_user, *args, **kwargs):
+        """
+        Saves the object and creates an audit object.
+
+        Args:
+            acting_user (User):
+                The user who made the change to the model. May be None if inapplicable.
+        """
+        before_obj = self.objects_for_audit().filter(id=self.id).first()
+        self.save(*args, **kwargs)
+        self.refresh_from_db()
+        before_dict = None
+        if before_obj is not None:
+            before_dict = before_obj.to_dict()
+
+        audit_kwargs = dict(
+            acting_user=acting_user, data_before=before_dict, data_after=self.to_dict()
+        )
+        audit_class = self.get_audit_class()
+        audit_kwargs[audit_class.get_related_field_name()] = self
+        audit_class.objects.create(**audit_kwargs)
+
+
 class SingletonModel(Model):
     """Model class for models representing tables that should only have a single record"""
 
