9757dc8ec0d98a1f3f3587f8c149f250e636b07f
Umar Asghar <mrumarasghar@gmail.com>
2022-06-24 20:42:21 +0500

    fixes cache_control_max_age_jitter issu (#74)

--- src/mitol/common/decorators.py
+++ src/mitol/common/decorators.py
@@ -1,4 +1,5 @@
 import random
+from django.conf import settings
 from functools import wraps
 
 from django.utils.cache import get_max_age, patch_cache_control
@@ -7,7 +8,7 @@ from django.utils.cache import get_max_age, patch_cache_control
 def cache_control_max_age_jitter(*args, **kwargs):
     def _cache_controller(viewfunc):
         @wraps(viewfunc)
-        def _cache_controlled(request, *args, **kwargs):
+        def _cache_controlled(request, *args, **kw):
             # Ensure argument looks like a request.
             if not hasattr(request, "META"):
                 raise TypeError(
@@ -15,12 +16,13 @@ def cache_control_max_age_jitter(*args, **kwargs):
                     "decorating a classmethod, be sure to use "
                     "@method_decorator."
                 )
-            response = viewfunc(request, *args, **kwargs)
+            response = viewfunc(request, *args, **kw)
             max_age = get_max_age(response)
+            if not max_age:
+                max_age = kwargs["max_age"]
             # add random delay upto 5 minutes
-            if max_age:
-                kwargs["max_age"] = max_age + random.randint(1, 600)
-                patch_cache_control(response, **kwargs)
+            kwargs["max_age"] = max_age + random.randint(1, 600)
+            patch_cache_control(response, **kwargs)
             return response
 
         return _cache_controlled
