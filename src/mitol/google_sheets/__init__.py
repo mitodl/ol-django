f8f4addabb0ae0ed6a8751b8cbf5b118a1751ba1
Anna Gavrilman <annagav@mit.edu>
2022-07-26 15:57:54 -0400

    Release mitol-django-google-sheets/v2.2.6

--- src/mitol/google_sheets/__init__.py
+++ src/mitol/google_sheets/__init__.py
@@ -1,5 +1,5 @@
 """ mitol.google_sheets """
 default_app_config = "mitol.google_sheets.apps.GoogleSheetsApp"
 
-__version__ = "2.2.5"
+__version__ = "2.2.6"
 __distributionname__ = "mitol-django-google-sheets"
