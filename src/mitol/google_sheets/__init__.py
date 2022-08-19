a3f5dfcf6bf62a14d551e50188657559f408db07
nathan <nathan@nathan-lev>
2022-08-19 15:58:26 -0400

    Release mitol-django-google-sheets/v2.4.1

--- src/mitol/google_sheets/__init__.py
+++ src/mitol/google_sheets/__init__.py
@@ -1,5 +1,5 @@
 """ mitol.google_sheets """
 default_app_config = "mitol.google_sheets.apps.GoogleSheetsApp"
 
-__version__ = "2.4.0"
+__version__ = "2.4.1"
 __distributionname__ = "mitol-django-google-sheets"
