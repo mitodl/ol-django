332cc033f402250fcea3491a3205d880007a1a3c
nathan <nathan@nathan-Latitude-7400>
2023-01-17 11:11:56 -0500

    Release mitol-django-hubspot-api/v1.1.0

--- src/mitol/hubspot_api/__init__.py
+++ src/mitol/hubspot_api/__init__.py
@@ -1,5 +1,5 @@
 """ mitol.hubspot_api """
 default_app_config = "mitol.hubspot_api.apps.HubspotApiApp"
 
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 __distributionname__ = "mitol-django-hubspot-api"
