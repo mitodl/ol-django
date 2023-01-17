f1fbfb269def9ed3e69b4f1c1fc84ed5f59a212d
nathan <nathan@nathan-Latitude-7400>
2023-01-17 11:13:02 -0500

    Release mitol-django-google-sheets/v2.6.0

--- src/mitol/google_sheets/__init__.py
+++ src/mitol/google_sheets/__init__.py
@@ -1,5 +1,5 @@
 """ mitol.google_sheets """
 default_app_config = "mitol.google_sheets.apps.GoogleSheetsApp"
 
-__version__ = "2.5.0"
+__version__ = "2.6.0"
 __distributionname__ = "mitol-django-google-sheets"
