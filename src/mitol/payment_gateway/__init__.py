1c99d4cf40df640b603b7ce45d187a655e647063
nathan <nathan@nathan-Latitude-7400>
2023-01-17 11:28:36 -0500

    Release mitol-django-payment-gateway/v1.8.0

--- src/mitol/payment_gateway/__init__.py
+++ src/mitol/payment_gateway/__init__.py
@@ -1,5 +1,5 @@
 """ mitol.openedx """
 default_app_config = "mitol.payment_gateway.apps.PaymentGatewayApp"
 
-__version__ = "1.7.1"
+__version__ = "1.8.0"
 __distributionname__ = "mitol-django-payment-gateway"
