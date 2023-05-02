38eda1edda4f5b735e2e246b5030e4e5ccb4c355
Anna Gavrilman <annagav@mit.edu>
2023-05-02 13:46:11 -0400

    fmt

--- src/mitol/google_sheets_deferrals/migrations/0001_initial.py
+++ src/mitol/google_sheets_deferrals/migrations/0001_initial.py
@@ -7,22 +7,29 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='DeferralRequest',
+            name="DeferralRequest",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_on', models.DateTimeField(auto_now_add=True)),
-                ('updated_on', models.DateTimeField(auto_now=True)),
-                ('form_response_id', models.IntegerField(db_index=True, unique=True)),
-                ('date_completed', models.DateTimeField(blank=True, null=True)),
-                ('raw_data', models.CharField(blank=True, max_length=300, null=True)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("created_on", models.DateTimeField(auto_now_add=True)),
+                ("updated_on", models.DateTimeField(auto_now=True)),
+                ("form_response_id", models.IntegerField(db_index=True, unique=True)),
+                ("date_completed", models.DateTimeField(blank=True, null=True)),
+                ("raw_data", models.CharField(blank=True, max_length=300, null=True)),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
     ]
