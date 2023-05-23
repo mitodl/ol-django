50d6fc4830c822bae076e68a12a25ba6677e4bb3
Anna Gavrilman <annagav@users.noreply.github.com>
2023-05-23 15:12:07 +0200

    Update version format for google-sheets-deferrals (#118)

--- src/mitol/google_sheets_deferrals/__init__.py
+++ src/mitol/google_sheets_deferrals/__init__.py
@@ -1,5 +1,5 @@
 """ mitol.google_sheets_deferrals """
 default_app_config = "mitol.google_sheets_deferrals.apps.GoogleSheetsDeferralsApp"
 
-__version__ = "0.0.0"
+__version__ = "2023.5.23"
 __distributionname__ = "mitol-django-google-sheets-deferrals"
