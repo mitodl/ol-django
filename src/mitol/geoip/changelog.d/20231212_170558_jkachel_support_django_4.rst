fcf758343f10cd11e837961ae27c7ca43099f567
James Kachel <jkachel@mit.edu>
2023-12-12 17:14:52 +0000

    Update reqs to support Django 4 and remove Django 2.2
    This removes the top end version restriction on Django and bumps the lowest version to 3.0. There are also updates to a couple of other libraries (Djoser, social-auth) as they were out of date and causing issues, and a small update to the testapp/utils.py as the middleware API has changed slightly in 4.0.

---
+++ src/mitol/geoip/changelog.d/20231212_170558_jkachel_support_django_4.rst
@@ -0,0 +1,37 @@
+.. A new scriv changelog fragment.
+..
+.. Uncomment the header that is right (remove the leading dots).
+..
+.. Removed
+.. -------
+..
+.. - A bullet item for the Removed category.
+..
+.. Added
+.. -----
+..
+.. - A bullet item for the Added category.
+..
+
+### Changed
+
+- Updated requirements and lockfiles to support Django 4. 
+- Removed support for Django 2.2.
+
+
+..
+.. Deprecated
+.. ----------
+..
+.. - A bullet item for the Deprecated category.
+..
+.. Fixed
+.. -----
+..
+.. - A bullet item for the Fixed category.
+..
+.. Security
+.. --------
+..
+.. - A bullet item for the Security category.
+..
--- tests/testapp/utils.py
+++ tests/testapp/utils.py
@@ -5,7 +5,9 @@ from django.contrib.sessions.middleware import SessionMiddleware
 
 def set_request_session(request, session_dict):
     """
-    Sets session variables on a RequestFactory object
+    Sets session variables on a RequestFactory object. Uses a fake response (empty
+    string) for compatibility with Django 4, which no longer defaults to None.
+    
     Args:
         request (WSGIRequest): A RequestFactory-produced request object (from RequestFactory.get(), et. al.)
         session_dict (dict): Key/value pairs of session variables to set
@@ -13,7 +15,7 @@ def set_request_session(request, session_dict):
     Returns:
         RequestFactory: The same request object with session variables set
     """
-    middleware = SessionMiddleware()
+    middleware = SessionMiddleware("")
     middleware.process_request(request)
     for key, value in session_dict.items():
         request.session[key] = value
