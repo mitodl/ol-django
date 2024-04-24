d20f73812dd130610f477ad3c070b365b95a3ad8
Collin Preston <collinpreston@users.noreply.github.com>
2024-04-24 17:21:23 -0400

    Update version month format (#153)
    * Update version month format
    
    * Add changelog

--- src/mitol/olposthog/__init__.py
+++ src/mitol/olposthog/__init__.py
@@ -1,5 +1,5 @@
 """ mitol.olposthog """
 default_app_config = "mitol.olposthog.apps.OlPosthog"
 
-__version__ = "2024.04.12.1"
+__version__ = "2024.4.12.1"
 __distributionname__ = "mitol-django-OlPosthog"
