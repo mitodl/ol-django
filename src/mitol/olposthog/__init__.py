865c9da96669612ec615f40b3b98804eeffd60e4
Collin Preston <collin_preston@yahoo.com>
2024-04-25 10:13:48 -0400

    Release mitol-olposthog/v2024.4.25

--- src/mitol/olposthog/__init__.py
+++ src/mitol/olposthog/__init__.py
@@ -1,5 +1,5 @@
 """ mitol.olposthog """
 default_app_config = "mitol.olposthog.apps.OlPosthog"
 
-__version__ = "2024.4.12.1"
+__version__ = "2024.4.25"
 __distributionname__ = "mitol-django-OlPosthog"
