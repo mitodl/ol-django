43c1dec9e42b1ad097d821b21890289ee70c79f4
Umar Hassan <umar.hassan8@gmail.com>
2025-02-13 05:45:25 +0500

    added media convert job

--- src/transcoding/mitol/transcoding/api.py
+++ src/transcoding/mitol/transcoding/api.py
@@ -1,7 +1,42 @@
 """API for the transcoding app."""
+from django.conf import settings
+import boto3
+import json
+import os
 
 
-def hello_world():
-    """Return Hello World!"""
-
-    return "Hello world!"
+def create_media_convert_job(video_source_key):
+    """Create a MediaConvert job for a Video"""
+    source_prefix = settings.DRIVE_S3_UPLOAD_PREFIX
+    client = boto3.client(
+        "mediaconvert",
+        region_name=settings.AWS_REGION,
+        endpoint_url=settings.VIDEO_S3_TRANSCODE_ENDPOINT,
+    )
+    with open(  # noqa: PTH123
+        os.path.join(  # noqa: PTH118
+           os.getcwd(), "config/mediaconvert.json"  # noqa: PTH109
+        ),
+        encoding="utf-8",
+    ) as job_template:
+        job_dict = json.loads(job_template.read())
+        job_dict["UserMetadata"]["filter"] = settings.VIDEO_TRANSCODE_QUEUE
+        job_dict["Queue"] = (
+            f"arn:aws:mediaconvert:{settings.AWS_REGION}:{settings.AWS_ACCOUNT_ID}:queues/{settings.VIDEO_TRANSCODE_QUEUE}"
+        )
+        job_dict["Role"] = (
+            f"arn:aws:iam::{settings.AWS_ACCOUNT_ID}:role/{settings.AWS_ROLE_NAME}"
+        )
+        destination = os.path.splitext(  # noqa: PTH122
+            video_source_key.replace(
+                source_prefix,
+                settings.VIDEO_S3_TRANSCODE_PREFIX,
+            )
+        )[0]
+        job_dict["Settings"]["OutputGroups"][0]["OutputGroupSettings"][
+            "FileGroupSettings"
+        ]["Destination"] = f"s3://{settings.AWS_STORAGE_BUCKET_NAME}/{destination}"
+        job_dict["Settings"]["Inputs"][0]["FileInput"] = (
+            f"s3://{settings.AWS_STORAGE_BUCKET_NAME}/{video_source_key}"
+        )
+        return client.create_job(**job_dict)
--- src/transcoding/mitol/transcoding/apps.py
+++ src/transcoding/mitol/transcoding/apps.py
@@ -10,18 +10,19 @@ class Transcoding(AppConfig):
 
     name = "mitol.transcoding"
     label = "transcoding"
-    verbose_name = "transcoding"
+    verbose_name = "Transcoding App"
+
+    required_settings = [
+        "VIDEO_TRANSCODE_QUEUE",
+        "VIDEO_S3_TRANSCODE_ENDPOINT",
+        "VIDEO_S3_TRANSCODE_PREFIX",
+        "DRIVE_S3_UPLOAD_PREFIX",
+        "AWS_STORAGE_BUCKET_NAME",
+        "AWS_REGION",
+        "AWS_ACCOUNT_ID",
+        "AWS_ROLE_NAME",
+    ]
 
     # necessary because this is a namespaced app
     path = os.path.dirname(os.path.abspath(__file__))  # noqa: PTH100, PTH120
 
-
-class TransitionalTranscoding(AppConfig):
-    """AppConfig for transitioning a project with an existing 'transcoding' app"""
-
-    name = "mitol.transcoding"
-    label = "transitional_transcoding"
-    verbose_name = "transcoding"
-
-    # necessary because this is a namespaced app
-    path = os.path.dirname(os.path.abspath(__file__))  # noqa: PTH100, PTH120
