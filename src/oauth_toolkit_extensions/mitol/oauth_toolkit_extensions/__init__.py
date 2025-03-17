66d8f0a86c9bbd891c825b259c7b1ca7dbf3ec9f
nathan <nathan@lilith>
2025-03-17 15:35:15 -0400

    Release mitol-django-oauth-toolkit-extensions/v2025.3.17

--- src/oauth_toolkit_extensions/mitol/oauth_toolkit_extensions/__init__.py
+++ src/oauth_toolkit_extensions/mitol/oauth_toolkit_extensions/__init__.py
@@ -2,5 +2,5 @@
 
 default_app_config = "mitol.oauth_toolkit_extensions.apps.OAuthToolkitExtensionsApp"
 
-__version__ = "2023.12.19.1"
+__version__ = "2025.3.17"
 __distributionname__ = "mitol-django-oauth-toolkit-extensions"
