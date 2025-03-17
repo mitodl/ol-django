e3bb2951d6c3dd079ec43f73d47ab6591859b13d
nathan <nathan@lilith>
2025-03-17 15:35:42 -0400

    Release mitol-django-google-sheets-refunds/v2025.3.17

--- src/google_sheets_refunds/mitol/google_sheets_refunds/__init__.py
+++ src/google_sheets_refunds/mitol/google_sheets_refunds/__init__.py
@@ -2,5 +2,5 @@
 
 default_app_config = "mitol.google_sheets_refunds.apps.GoogleSheetsRefundsApp"
 
-__version__ = "2025.2.27"
+__version__ = "2025.3.17"
 __distributionname__ = "mitol-django-google-sheets-refunds"
