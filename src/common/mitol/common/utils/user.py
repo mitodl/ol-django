2f7342c524646fda02affb8d1025025b410d1500
asajjad2 <areebsajjad21@gmail.com>
2025-06-05 18:15:20 +0500

    style: fix line overflow issues]

--- src/common/mitol/common/utils/user.py
+++ src/common/mitol/common/utils/user.py
@@ -1,4 +1,4 @@
-"""User utilities"""
+"""User utilities."""
 
 import logging
 import re
@@ -21,8 +21,8 @@ log = logging.getLogger(__name__)
 
 
 def _reformat_for_username(string):
-    """
-    Removes/substitutes characters in a string to make it suitable as a username value
+    """Removes/substitutes characters in a string to make it suitable as a
+    username value.
 
     Args:
         string (str): A string
@@ -31,19 +31,20 @@ def _reformat_for_username(string):
     """
     cleaned_string = re.sub(USERNAME_INVALID_CHAR_PATTERN, "", string)
     cleaned_string = re.sub(
-        USERNAME_TURKISH_I_CHARS, USERNAME_TURKISH_I_CHARS_REPLACEMENT, cleaned_string
+        USERNAME_TURKISH_I_CHARS, USERNAME_TURKISH_I_CHARS_REPLACEMENT,
+        cleaned_string,
     )
     return (
-        re.sub(USERNAME_SEPARATOR_REPLACE_PATTERN, USERNAME_SEPARATOR, cleaned_string)
+        re.sub(USERNAME_SEPARATOR_REPLACE_PATTERN, USERNAME_SEPARATOR,
+               cleaned_string)
         .lower()
         .strip(USERNAME_SEPARATOR)
     )
 
 
 def is_duplicate_username_error(exc):
-    """
-    Return True if the given exception indicates that there was an attempt to save a User record with an
-    already-existing username.
+    """Return True if the given exception indicates that there was an attempt
+    to save a User record with an already-existing username.
 
     Args:
         exc (Exception): An exception
@@ -51,16 +52,21 @@ def is_duplicate_username_error(exc):
     Returns:
         bool: Whether or not the exception indicates a duplicate username error
     """
-    return re.search(r"\(username\)=\([^\s]+\) already exists", str(exc)) is not None
+    return re.search(
+        r"\(username\)=\([^\s]+\) already exists",
+        str(exc)) is not None
 
 
 def _find_available_username(
-    initial_username_base, model=None, username_field="username", max_length=USERNAME_MAX_LEN
+    initial_username_base,
+    model=None,
+    username_field="username",
+    max_length=USERNAME_MAX_LEN,
 ):
-    """
-    Return a username with the lowest possible suffix given some base username. If the applied suffix
-    makes the username longer than the username max length, characters are removed from the
-    right of the username to make room.
+    """Return a username with the lowest possible suffix given some
+    base username. If the applied suffix makes the username longer than the
+    username max length, characters are removed from the right of the username
+    to make room.
 
     EXAMPLES:
     initial_username_base = "johndoe"
@@ -69,83 +75,93 @@ def _find_available_username(
     initial_username_base = "johndoe"
         Existing usernames = "johndoe", "johndoe1" through "johndoe5"
         Return value = "johndoe6"
-    initial_username_base = "abcdefghijklmnopqrstuvwxyz" (26 characters, assuming 26 character max)
+    initial_username_base = "abcdefghijklmnopqrstuvwxyz"
+        (26 characters, assuming 26 character max)
         Existing usernames = "abcdefghijklmnopqrstuvwxyz"
-        Return value = "abcdefghijklmnopqrstuvwxy1"
-    initial_username_base = "abcdefghijklmnopqrstuvwxy" (25 characters long, assuming 26 character max)
+        Return value = "abcdefghijklmnopqrstuvwxy1"  # pragma: allowlist secre
+    initial_username_base = "abcdefghijklmnopqrstuvwxy"
+        (25 characters long, assuming 26 character max)
         Existing usernames = "abc...y", "abc...y1" through "abc...y9"
-        Return value = "abcdefghijklmnopqrstuvwx10"
+        Return value = "abcdefghijklmnopqrstuvwx10"  # pragma: allowlist secre
 
     Args:
          initial_username_base (str): Base username to start with
          model: The model class to query (defaults to User model)
-         username_field (str): The field name to use for username queries (defaults to 'username')
-         max_length (int): The maximum allowed username length (defaults to USERNAME_MAX_LEN)
+         username_field (str): The field name to use for username queries
+            (defaults to 'username')
+         max_length (int): The maximum allowed username length
+            (defaults to USERNAME_MAX_LEN)
     Returns:
         str: An available username
     """
     if model is None:
         model = get_user_model()
 
-    # Keeps track of the number of characters that must be cut from the username to be less than
-    # the username max length when the suffix is applied.
+    # Keeps track of the number of characters that must be cut from the
+    # username to be less than the username max length when the 
+    # suffix is applied.
     letters_to_truncate = 0 if len(initial_username_base) < max_length else 1
-    # Any query for suffixed usernames could come up empty. The minimum suffix will be added to
-    # the username in that case.
+    # Any query for suffixed usernames could come up empty. The minimum suffix
+    # will be added to the username in that case.
     current_min_suffix = 1
     while letters_to_truncate < len(initial_username_base):  # noqa: RET503
         username_base = initial_username_base[
-            0 : len(initial_username_base) - letters_to_truncate
+            0: len(initial_username_base) - letters_to_truncate
         ]
-        # Find usernames that match the username base and have a numerical suffix, then find the max suffix
+        # Find usernames that match the username base and have a numerical
+        # suffix, then find the max suffix
         filter_kwargs = {f"{username_field}__regex": rf"{username_base}[0-9]+"}
-        existing_usernames = model.objects.filter(
-            **filter_kwargs
-        ).values_list(username_field, flat=True)
+        existing_usernames = model.objects.filter(**filter_kwargs).values_list(
+            username_field, flat=True
+        )
         max_suffix = max_or_none(
-            int(re.search(r"\d+$", username).group()) for username in existing_usernames
+            int(re.search(r"\d+$", username).group())
+            for username in existing_usernames
         )
         if max_suffix is None:
             return "".join([username_base, str(current_min_suffix)])
         else:
             next_suffix = max_suffix + 1
             candidate_username = "".join([username_base, str(next_suffix)])
-            # If the next suffix adds a digit and causes the username to exceed the character limit,
-            # keep searching.
+            # If the next suffix adds a digit and causes the username to
+            # exceed the character limit, keep searching.
             if len(candidate_username) <= max_length:
                 return candidate_username
-        # At this point, we know there are no suffixes left to add to this username base that was tried,
-        # so we will need to remove letters from the end of that username base to make room for a longer
-        # suffix.
+        # At this point, we know there are no suffixes left to add to this
+        # username base that was tried,
+        # so we will need to remove letters from the end of that username base
+        # to make room for a longer suffix.
         letters_to_truncate = letters_to_truncate + 1
         available_suffix_digits = max_length - (
             len(initial_username_base) - letters_to_truncate
         )
-        # If there is space for 4 digits for the suffix, the minimum value it could be is 1000, or 10^3
+        # If there is space for 4 digits for the suffix, the minimum value it
+        # could be is 1000, or 10^3
         current_min_suffix = 10 ** (available_suffix_digits - 1)
 
 
 def usernameify(full_name, email="", max_length=USERNAME_MAX_LEN):
-    """
-    Public API for username generation
-    Generate a username based on a full name, or an email address as a fallback.
+    """Public API for username generation Generate a username based on a
+    full name, or an email address as a fallback.
 
     Args:
         full_name (str): A full name (i.e.: User.name)
-        email (str): An email address to use as a fallback if the full name produces
-            a blank username
-        max_length (int): The maximum allowed username length (defaults to USERNAME_MAX_LEN)
+        email (str): An email address to use as a fallback if the full name
+            produces a blank username
+        max_length (int): The maximum allowed username length
+            (defaults to USERNAME_MAX_LEN)
     Returns:
         str: A generated username
     Raises:
-        ValueError: Raised if generated username was blank after trying both the
-            full name and email
+        ValueError: Raised if generated username was blank after trying both 
+            the full name and email
     """
     username = _reformat_for_username(full_name)
 
     if not username and email:
         log.error(
-            "User's full name could not be used to generate a username (full name: '%s'). "
+            "User's full name could not be used to generate a username "
+            "(full name: '%s'). "
             "Trying email instead...",
             full_name,
         )
@@ -167,23 +183,24 @@ def create_user_with_generated_username(
     model=None,
     attempts_limit=USERNAME_COLLISION_ATTEMPTS,
 ):
-    """
-    Public API for user creation with auto-generated username
-    Creates a User with a given username, and if there is a User that already has that username,
-    finds an available username and reattempts the User creation.
+    """Public API for user creation with auto-generated username
+    Creates a User with a given username, and if there is a User that already
+    has that username, finds an available username and
+    reattempts the User creation.
 
     Args:
         serializer: A serializer instance that has been instantiated
             with user data and has passed initial validation
-        initial_username (str): The initial username to attempt to save the User with
+        initial_username (str): The initial username to attempt to
+            save the User with
         username_field (str): The field name to use for the username
         max_length (int): The maximum allowed username length
         model: The model class to pass to find_username_func (defaults to None)
         attempts_limit (int): Maximum number of attempts to create user
 
     Returns:
-        User or None: The created User (or None if the User could not be created in the
-            number of retries allowed)
+        User or None: The created User (or None if the User could not be
+        created in the number of retries allowed)
     """
     created_user = None
     username = initial_username
@@ -202,7 +219,7 @@ def create_user_with_generated_username(
                 initial_username,
                 model=model,
                 username_field=username_field,
-                max_length=max_length
+                max_length=max_length,
             )
         finally:
             attempts += 1
