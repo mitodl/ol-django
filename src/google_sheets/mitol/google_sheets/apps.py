b225a7b71e1441a03286f80d9c5260467b5c998f
Anna <annagav@mit.edu>
2025-06-12 12:19:00 -0400

    Adding missing default_auto_field

--- src/google_sheets/mitol/google_sheets/apps.py
+++ src/google_sheets/mitol/google_sheets/apps.py
@@ -8,6 +8,9 @@ from mitol.common.apps import BaseApp
 class GoogleSheetsApp(BaseApp):
     """Default configuration for the Google Sheets app"""
 
+    # Explicitly set default auto field type to avoid migrations in Django 3.2+
+    default_auto_field = "django.db.models.BigAutoField"
+
     name = "mitol.google_sheets"
     label = "google_sheets"
     verbose_name = "Google Sheets"
