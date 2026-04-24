117cece78dde334796b29321434416229c006072
Anna <annagav@mit.edu>
2026-04-24 13:13:03 -0400

    middleware override call

--- src/apigateway/mitol/apigateway/middleware.py
+++ src/apigateway/mitol/apigateway/middleware.py
@@ -18,6 +18,28 @@ log = logging.getLogger(__name__)
 class ApisixUserMiddleware(RemoteUserMiddleware):
     """Checks for and processes APISIX-specific headers."""
 
+    def __call__(self, request):
+        """Run auth processing and set post-logout redirect cookie on response."""
+        if settings.MITOL_APIGATEWAY_DISABLE_MIDDLEWARE:
+            return self.get_response(request)
+
+        response = super().__call__(request)
+
+        next_param = request.GET.get("next") if request.GET else None
+        if next_param:
+            log.debug(
+                "ApisixUserMiddleware.__call__: Setting next cookie to %s",
+                next_param,
+            )
+            response.set_cookie("next", next_param, max_age=30, secure=False)
+
+        log.debug(
+            "ApisixUserMiddleware.__call__: Next cookie is %s",
+            response.cookies.get("next"),
+        )
+
+        return response
+
     def process_request(self, request):
         """
         Modify the header to contain username, pass off to RemoteUserMiddleware.
@@ -26,29 +48,13 @@ class ApisixUserMiddleware(RemoteUserMiddleware):
         log.debug("ApisixUserMiddleware.process_request: started")
 
         if settings.MITOL_APIGATEWAY_DISABLE_MIDDLEWARE:
-            return self.get_response(request)
+            return
 
         if request.META.get(settings.MITOL_APIGATEWAY_USERINFO_HEADER_NAME):
             new_header = get_user_id_from_userinfo_header(request)
             request.META["REMOTE_USER"] = new_header
 
         super().process_request(request)
-        response = self.get_response(request)
-
-        next_param = request.GET.get("next", None) if request.GET else None
-        if next_param:
-            log.debug(
-                "ApisixUserMiddleware.process_request: Setting next cookie to %s",
-                next_param,
-            )
-            response.set_cookie("next", next_param, max_age=30, secure=False)
-
-        log.debug(
-            "ApisixUserMiddleware.process_request: Next cookie is %s",
-            response.cookies.get("next"),
-        )
-
-        return response
 
 
 class PersistentApisixUserMiddleware(
