5a5b5a1faec5efe201308861c32850c6114f5c48
Nathan Levesque <nlevesq@mit.edu>
2026-07-02 16:31:48 -0400

    Remove support for touchstone auth

--- testapp/main/urls.py
+++ testapp/main/urls.py
@@ -1,7 +1,5 @@
 """testapp URL Configuration"""
 
-import sys
-
 from django.contrib import admin
 from django.urls import include, path
 from oauth2_provider.urls import base_urlpatterns
@@ -18,7 +16,6 @@ urlpatterns = [
     path("api/", include("mitol.google_sheets.urls")),
     path("api/", include("mitol.mail.urls")),
     path("", include("mitol.authentication.urls.djoser_urls")),
-    path("", include("social_django.urls", namespace="social")),
     path(
         "oauth2/",
         include((base_urlpatterns, "oauth2_provider"), namespace="oauth2_provider"),
@@ -28,8 +25,3 @@ urlpatterns = [
     path("", include("mitol.scim.urls")),
     path("", include("mitol.apigateway.urls")),
 ]
-
-if sys.version_info < (3, 13):
-    urlpatterns += [
-        path("", include("mitol.authentication.urls.saml")),
-    ]
