a620c1f97c43a9b4fac55466868271dc762140bd
Asad Ali <asad.ali@arbisoft.com>
2026-08-05 16:44:32 +0500

    changes and pre-commit fixes

--- src/payment_gateway/mitol/payment_gateway/api.py
+++ src/payment_gateway/mitol/payment_gateway/api.py
@@ -3,6 +3,7 @@ API for the Payment Gateway
 """
 
 import abc
+import contextlib
 import hashlib
 import hmac
 import json
@@ -641,10 +642,8 @@ class CyberSourcePaymentGateway(
             # exception rather than masking the original error with a KeyError.
             exception_body = None
             if hasattr(ex, "body") and ex.body:
-                try:
+                with contextlib.suppress(ValueError, TypeError):
                     exception_body = json.loads(ex.body)
-                except (ValueError, TypeError):
-                    pass
 
             # Special case for request failure when DUPLICATE_REQUEST
             if (
