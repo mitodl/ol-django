7f793e28191e7d524faba522213ef3bb64e429a2
James Kachel <jkachel@mit.edu>
2026-08-07 10:25:21 -0500

    Add some really basic dataclasses, add a failure test

--- tests/payment_gateway/factories.py
+++ tests/payment_gateway/factories.py
@@ -1,9 +1,11 @@
 # ruff: noqa: CPY001
 """Test factories for Payment Gateway."""
 
+import dataclasses
+from datetime import datetime
+
 import faker
 import pytz
-import stripe
 from django.conf import settings
 from factory import Factory, LazyAttribute, SubFactory
 from factory.django import DjangoModelFactory
@@ -39,6 +41,90 @@ class StripeWebhookSecretRouteFactory(DjangoModelFactory):
         model = StripeWebhookSecretRoute
 
 
+@dataclasses.dataclass
+class SimpleStripeEvent:
+    """A very basic representation of a Stripe event, suitable for testing."""
+
+    id: str | None = None
+    created: datetime | None = None
+    type: str | None = None
+    object: str = "event"
+    data: dict = dataclasses.field(default_factory=dict)
+    request: dict = dataclasses.field(default_factory=dict)
+    livemode: bool = False
+    api_version: str = "2026-06-24.dahlia"
+    pending_webhooks: int = 0
+
+    def to_dict(self, *, for_json: bool = False):
+        """Return the event as a dict"""
+
+        return {
+            "id": self.id,
+            "created": self.created.isoformat()
+            if self.created and for_json
+            else self.created,
+            "type": self.type,
+            "object": self.object,
+            "data": self.data,
+            "request": self.request,
+            "livemode": self.livemode,
+            "api_version": self.api_version,
+            "pending_webhooks": self.pending_webhooks,
+        }
+
+    def get(self, field):
+        """Get the value for the field"""
+
+        return getattr(self, field)
+
+
+@dataclasses.dataclass
+class SimpleStripeCheckoutSession:
+    """A basic representation of a Stripe checkout session, suitable for testing."""
+
+    id: str | None = None
+    created: datetime | None = None
+    object: str = "checkout.session"
+    payment_intent: str | dict = ""
+    mode: str = "payment"
+    ui_mode: str = "hosted_page"
+    payment_status: str = "paid"
+    status: str = ""
+    amount_total: int = 0
+    amount_subtotal: int = 0
+    client_reference_id: str = ""
+    success_url: str = ""
+    cancel_url: str = ""
+    currency: str = "usd"
+
+    def to_dict(self, *, for_json: bool = False):
+        """Return the session as a dict"""
+
+        return {
+            "id": self.id,
+            "created": self.created.isoformat()
+            if self.created and for_json
+            else self.created,
+            "object": self.object,
+            "payment_intent": self.payment_intent,
+            "mode": self.mode,
+            "ui_mode": self.ui_mode,
+            "payment_status": self.payment_status,
+            "status": self.status,
+            "amount_total": self.amount_total,
+            "amount_subtotal": self.amount_subtotal,
+            "client_reference_id": self.client_reference_id,
+            "success_url": self.success_url,
+            "cancel_url": self.cancel_url,
+            "currency": self.currency,
+        }
+
+    def get(self, field, default=None):
+        """Get the value for the field"""
+
+        return getattr(self, field, default)
+
+
 class StripeAbstractEventFactory(Factory):
     """Abstract factory to set some defaults for Stripe events."""
 
@@ -50,6 +136,7 @@ class StripeAbstractEventFactory(Factory):
         """Factory meta opts"""
 
         abstract = True
+        model = SimpleStripeEvent
 
 
 class StripeSimpleCheckoutSessionFactory(Factory):
@@ -79,7 +166,7 @@ class StripeSimpleCheckoutSessionFactory(Factory):
     class Meta:
         """Factory meta opts"""
 
-        model = stripe.checkout.Session
+        model = SimpleStripeCheckoutSession
 
 
 class StripeCheckoutSessionEventFactory(StripeAbstractEventFactory):
@@ -93,4 +180,4 @@ class StripeCheckoutSessionEventFactory(StripeAbstractEventFactory):
     class Meta:
         """Factory meta opts"""
 
-        model = stripe.Event
+        model = SimpleStripeEvent
